aafd8b110474b3e1c2a0e1b9fd687d4b3e0d25f4
Brice Mabillard <bricemabi@gmail.com>
2025-06-04 23:09:23 +0200

    fix

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,17 +1,15 @@
-import { createApp } from "vue";
-import App from "@/App.vue";
-import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+import { createApp } from 'vue'
+import App from '@/App.vue'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
-const application = createApp(App);
+const application = createApp(App)
 
-application.use(
-    Vue3Toastify,
-    {
-        toastClassName: "my-toast",
-        bodyClassName: "my-toast-body",
-        autoClose: 3000,
-        theme: "light",
-    } as ToastContainerOptions,);
+application.use(Vue3Toastify, {
+  toastClassName: 'my-toast',
+  bodyClassName: 'my-toast-body',
+  autoClose: 3000,
+  theme: 'light',
+} as ToastContainerOptions)
 
-export default application;
+export default application
